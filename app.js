bee9053c9dd65a1717ca9e5b92001fc853b8cf78
nithu2109 <nithish.vagu219@gmail.com>
2021-10-07 21:28:56 +0530

    Updated Update and Delete Operations

--- app.js
+++ app.js
@@ -1,29 +1,16 @@
 const taskContainer=document.querySelector(".taskcontainer");
 let globalTaskData =[];
 
-const addNewCard=()=>{
-    const taskData ={
-        id:`${Date.now()}`,
-        url:document.getElementById("imgurl").value,
-        title:document.getElementById("tasktitle").value,
-        type:document.getElementById("tasktype").value,
-        description:document.getElementById("taskdescription").value,
-    };
-       //pushing data to globalTaskData array
-    globalTaskData.push(taskData);
-       //updating the local storage
-    localStorage.setItem("taskyCA",JSON.stringify({cards:globalTaskData}));
-
-    //generate  html code 
-    const newCard=`<div id=${taskData.id} class="col-md-6 col-lg-4 mt-3" >
+const generateHTML =(taskData)=>
+  `<div id=${taskData.id} class="col-md-6 col-lg-4 mt-3" >
     <div class="card">
       <div class="card-header ">
         <div class="d-flex justify-content-end gap-2">
          <button type="button" class="btn btn-outline-info" onclick="editTask(this)">
            <i class="fas fa-pencil-alt"></i>
          </button>
-         <button type="button" class="btn btn-outline-danger"  onclick="deleteTask(this)">
-           <i class="far fa-trash-alt" ></i>
+         <button type="button" class="btn btn-outline-danger" name=${taskData.id} onclick="deleteTask(this)">
+           <i class="far fa-trash-alt"  ></i>
          </button>
         </div> 
       </div>
@@ -36,14 +23,36 @@ const addNewCard=()=>{
         <span class="badge bg-primary">${taskData.type}</span>
       </div>
       <div class="card-footer">
-        <button class="btn btn-outline-primary float-end">
+        <button class="btn btn-outline-primary float-end" name=${taskData.id}>
           Open Task
         </button>
       </div>
     </div>
-   </div>`
+   </div>`;
+    
+const injectDOM=(content)=>
+taskContainer.insertAdjacentHTML("beforeend",content);
+
+const saveToLocalStorage = ()=>
+localStorage.setItem("taskyCA", JSON.stringify({ card: globalTaskData }));
+ 
+const addNewCard=()=>{
+    const taskData ={
+        id:`${Date.now()}`,
+        url:document.getElementById("imgurl").value,
+        title:document.getElementById("tasktitle").value,
+        type:document.getElementById("tasktype").value,
+        description:document.getElementById("taskdescription").value,
+    };
+       //pushing data to globalTaskData array
+      globalTaskData.push(taskData);
+       //updating the local storage
+     saveToLocalStorage();
+
+    //generate  html code 
+    const newCard=generateHTML(taskData);
     //Inject it to dom
-     taskContainer.insertAdjacentHTML('beforeend',newCard);
+     injectDOM(newCard);
     //clear form
     document.getElementById("imgurl").value="";
     document.getElementById("tasktitle").value="";
@@ -61,42 +70,62 @@ const loadExistingCards =()=>{
     //if data in  local storage exist Parse that data
     if(!getData) return;
     const taskCards = JSON.parse(getData);
-    globalTaskData=taskCards.cards;
+    globalTaskData=taskCards.card;
 
 
     //generate html code for that data
     globalTaskData.map((taskData)=>{
-        const newCard=`<div id=${taskData.id} class="col-md-6 col-lg-4 mt-3" >
-        <div class="card">
-          <div class="card-header ">
-            <div class="d-flex justify-content-end gap-2">
-             <button type="button" class="btn btn-outline-info" onclick="editTask(this)">
-               <i class="fas fa-pencil-alt"></i>
-             </button>
-             <button type="button" class="btn btn-outline-danger"  onclick="deleteTask(this)">
-               <i class="far fa-trash-alt" ></i>
-             </button>
-            </div> 
-          </div>
-          <div>
-            <img src=${taskData.url} class="card-img-top" alt="image">
-          </div>
-          <div class="card-body">
-            <h5 class="card-title">${taskData.title}</h5>
-            <p class="card-text">${taskData.description}</p>
-            <span class="badge bg-primary">${taskData.type}</span>
-          </div>
-          <div class="card-footer">
-            <button class="btn btn-outline-primary float-end">
-              Open Task
-            </button>
-          </div>
-        </div>
-       </div>`;
+        const newCard=generateHTML(taskData);
 
        //inject that data to Dom
-    taskContainer.insertAdjacentHTML("beforeend",newCard);
+        injectDOM(newCard);
     });
     return;
     
 };
+
+const deleteTask = (e) => {
+  const targetID = e.getAttribute("name");
+  globalTaskData = globalTaskData.filter((cardData) => cardData.id!==targetID);
+  saveToLocalStorage();
+  window.location.reload();
+}
+
+const editTask = (e) => {
+  const targetID = e.getAttribute("name");
+  
+  e.parentNode.parentNode.parentNode.childNodes[5].childNodes[1].setAttribute("contenteditable", "true")
+  e.parentNode.parentNode.parentNode.childNodes[5].childNodes[3].setAttribute("contenteditable", "true")
+  e.parentNode.parentNode.parentNode.childNodes[5].childNodes[5].setAttribute("contenteditable", "true")
+
+  e.parentNode.parentNode.parentNode.childNodes[7].childNodes[1].setAttribute("onclick", " saveEditTask(this)")
+  e.parentNode.parentNode.parentNode.childNodes[7].childNodes[1].innerHTML = "SAVE CHANGES"
+}
+
+const saveEditTask=(e)=>{
+  const targetID=e.getAttribute("name");
+  const updatedData = {
+    title: e.parentNode.parentNode.childNodes[5].childNodes[1].innerHTML,
+    type: e.parentNode.parentNode.childNodes[5].childNodes[3].innerHTML,
+    description:e.parentNode.parentNode.childNodes[5].childNodes[5].innerHTML,
+  };
+
+  
+  const updateGlobalTasks = globalTaskData.map((task) => {
+    if (task.id === targetID) {
+    
+      return { ...task, ...updatedData };
+    }
+    return task;
+  });
+
+  globalTaskData = updateGlobalTasks;
+
+  saveToLocalStorage();
+  e.parentNode.parentNode.childNodes[5].childNodes[1].setAttribute("contenteditable", "false");
+  e.parentNode.parentNode.childNodes[5].childNodes[3].setAttribute("contenteditable", "false");
+  e.parentNode.parentNode.childNodes[5].childNodes[5].setAttribute("contenteditable", "false");
+  e.parentNode.parentNode.childNodes[7].childNodes[1].innerHTML = "Open Task"
+
+
+}
